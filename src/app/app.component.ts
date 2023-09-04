88ef31fa2c9627d42e36e56b57da4f556784fc45
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2023-09-04 19:05:02 -0400

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ecommerce';
+  isNavbarOpen: boolean = false;
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
 }
